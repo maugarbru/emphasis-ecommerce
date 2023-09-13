55e811f9463741c26b5437f0cf615eb353b68ed6
MGBrunal <mauriciogarzonb@gmail.com>
2023-09-13 00:56:56 -0500

    feat(frontend) changes carrito to table

---
+++ frontend/src/components/ShoppingCart/ListItem.tsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react';
+import { HiPlus, HiMinus, HiX } from 'react-icons/hi';
+import { useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+
+import { APIbaseURL } from 'src/core/config';
+import { ItemCarrito } from 'src/core/types';
+import { setCarrito } from 'src/core/store/slices/userData';
+import classNames from 'classnames';
+
+type ListItemProps = {
+  carrito: ItemCarrito[];
+  item: ItemCarrito;
+};
+
+const ListItem = ({ carrito, item }: ListItemProps): React.JSX.Element => {
+  const dispatch = useDispatch();
+  const [subtotal, setSubtotal] = useState(0);
+
+  const quitarItem = (item: ItemCarrito) => {
+    const newCarrito = carrito.filter(
+      (i) => i.producto.id !== item.producto.id,
+    );
+    dispatch(setCarrito(newCarrito));
+  };
+
+  const actualizarItem = (item: ItemCarrito, cambio: number) => {
+    const newCarrito = [...carrito];
+    const itemParaActualizar = newCarrito.findIndex(
+      (i) => i.producto.id === item.producto.id,
+    );
+    const itemActualizado: ItemCarrito = {
+      producto: newCarrito[itemParaActualizar].producto,
+      cantidad: newCarrito[itemParaActualizar].cantidad + cambio,
+    };
+    newCarrito[itemParaActualizar] = itemActualizado;
+    dispatch(setCarrito(newCarrito));
+  };
+
+  useEffect(() => {
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+    const body = {
+      idProducto: item.producto.id,
+      cantidad: item.cantidad,
+    };
+    fetch(APIbaseURL + 'carrito/subtotal', {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify(body),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        const { data, success, error } = result;
+        if (success) {
+          setSubtotal(data.subtotal);
+        } else {
+          toast.error(error.message);
+        }
+      });
+  }, [item]);
+
+  return (
+    <tr className="border-b last:border-none">
+      <td className="font-bold pl-5">{item.producto.nombre}</td>
+      <td className="text-right">{`$${item.producto.precio_unitario}`}</td>
+      <td className="flex justify-between items-center text-black mx-5">
+        <button
+          className={classNames(
+            'flex justify-center items-center rounded-lg border',
+            item.cantidad > 1
+              ? 'border-amber-500 text-amber-500'
+              : 'border-red-500 text-red-500',
+          )}
+          onClick={() => {
+            if (item.cantidad > 1) {
+              actualizarItem(item, -1);
+            } else {
+              quitarItem(item);
+            }
+          }}
+        >
+          {item.cantidad > 1 ? (
+            <HiMinus className="h-5 w-5" />
+          ) : (
+            <HiX className="h-5 w-5" />
+          )}
+        </button>
+        <p className="align-middle font-bold">{item.cantidad}</p>
+        <button
+          className="flex justify-center items-center rounded-lg border border-green-600 text-green-600 disabled:border-gray-300 disabled:text-gray-300"
+          disabled={item.cantidad >= item.producto.unidades_disponibles}
+          onClick={() => actualizarItem(item, 1)}
+        >
+          <HiPlus className="h-5 w-5" />
+        </button>
+      </td>
+      <td className="font-bold text-cyan-500 text-right pr-5">{`$${subtotal}`}</td>
+    </tr>
+  );
+};
+
+export default ListItem;
--- frontend/src/components/ShoppingCart/ShoppingCart.tsx
+++ frontend/src/components/ShoppingCart/ShoppingCart.tsx
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { HiShoppingCart, HiOutlineTrash } from 'react-icons/hi';
 import { Popover, Transition } from '@headlessui/react';
 import { toast } from 'react-toastify';
 import { useSelector, useDispatch } from 'react-redux';
 
-import ItemsList from './ItemsList';
+import ListItem from './ListItem';
+import { APIbaseURL } from 'src/core/config';
 import { RootState } from 'src/core/store';
 import { setCarrito } from 'src/core/store/slices/userData';
 
 const ShoppingCart = (): React.JSX.Element => {
   const dispatch = useDispatch();
-  const { carrito } = useSelector((state: RootState) => state.userData);
+  const { carrito, user } = useSelector((state: RootState) => state.userData);
+  const [total, setTotal] = useState(0);
 
   const limpiarCarrito = () => {
     dispatch(setCarrito([]));
     toast.success('Carrito limpio');
   };
+
+  useEffect(() => {
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+    const body = {
+      items: carrito.map((item) => ({
+        idProducto: item.producto.id,
+        cantidad: item.cantidad,
+      })),
+      idUsuario: user.id,
+    };
+    fetch(APIbaseURL + 'carrito/total', {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify(body),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        const { data, success, error } = result;
+        if (success) {
+          setTotal(data.total);
+        } else {
+          toast.error(error.message);
+        }
+      });
+  }, [carrito]);
 
   return (
     <Popover>
@@ -29,6 +57,7 @@ const ShoppingCart = (): React.JSX.Element => {
               </span>
             )}
           </Popover.Button>
+
           <Transition
             show={open}
             enter="transition duration-200 ease-out"
@@ -38,21 +67,54 @@ const ShoppingCart = (): React.JSX.Element => {
             leaveFrom="transform scale-100 opacity-100"
             leaveTo="transform scale-50 opacity-0"
           >
-            <Popover.Panel className="absolute right-2.5 top-2.5 w-96 h-[80vh] origin-top-right bg-white border border-black rounded-lg shadow-lg">
-              <div className="flex flex-col w-full h-full justify-between items-center space-y-4">
+            <Popover.Panel className="absolute right-2.5 top-2.5 w-[80vw] h-[80vh] origin-top-right bg-white border border-black rounded-lg shadow-lg">
+              <div className="flex flex-col w-full h-full justify-between items-center space-y-2">
                 <h1 className="text-2xl font-bold p-5">Carrito de compra</h1>
-                <ItemsList items={carrito} />
-                <button
-                  className="flex justify-center items-center border border-red-500 text-red-500 rounded-lg p-1 drop-shadow-lg hover:bg-red-200 disabled:bg-gray-200 disabled:text-gray-500 disabled:border-gray-500"
-                  onClick={limpiarCarrito}
-                  disabled={carrito?.length == 0}
-                >
-                  Limpiar carrito
-                  <HiOutlineTrash className="h-5 w-5 ml-2" />
-                </button>
+
+                {carrito?.length ? (
+                  <div className="w-full grow overflow-y-auto">
+                    <table className="table-auto w-full">
+                      <thead className="bg-cyan-100 shadow-lg">
+                        <tr>
+                          <th>Producto</th>
+                          <th>Precio Unitario</th>
+                          <th>Cantidad</th>
+                          <th>Subtotal</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {carrito.map((item) => (
+                          <ListItem
+                            key={item.producto.id}
+                            carrito={carrito}
+                            item={item}
+                          />
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                ) : (
+                  <div className="flex justify-center items-center italic font-bold text-sm text-red-500 w-full">
+                    <p>No hay productos añadidos al carrito</p>
+                  </div>
+                )}
+
                 <div className="flex justify-between items-center w-full border-black border-t p-5">
-                  <p className="text-2xl font-bold">Total:</p>
-                  <p className="text-2xl font-bold text-cyan-500">$0</p>
+                  <button
+                    className="flex justify-center items-center border border-red-500 text-red-500 rounded-lg p-1 drop-shadow-lg hover:bg-red-200 disabled:bg-gray-200 disabled:text-gray-500 disabled:border-gray-500"
+                    onClick={limpiarCarrito}
+                    disabled={carrito?.length == 0}
+                  >
+                    Limpiar carrito
+                    <HiOutlineTrash className="h-5 w-5 ml-2" />
+                  </button>
+
+                  <div className="flex space-x-4">
+                    <p className="text-2xl font-bold">Total:</p>
+                    <p className="text-2xl font-bold text-cyan-500">
+                      {`$${total}`}
+                    </p>
+                  </div>
                 </div>
               </div>
             </Popover.Panel>
