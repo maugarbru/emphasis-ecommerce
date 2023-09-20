d4b6cd5c7540be97bf77135cddca6a4167098fc5
MGBrunal <mauriciogarzonb@gmail.com>
2023-09-19 21:49:49 -0500

    feat(api) adds reglas para el precio

---
+++ api/src/core/rules/index.ts
@@ -0,0 +1,43 @@
+import { Producto, ReglaPrecio, RespuestaRegla } from '../util/types';
+
+export const reglasDisponibles: ReglaPrecio[] = [
+  {
+    nombre: 'Producto Normal',
+    identificador: 'EA',
+    calcularPrecio: (producto, cantidad) => {
+      return producto.precio_unitario * cantidad;
+    },
+  },
+  {
+    nombre: 'Producto Peso',
+    identificador: 'WE',
+    calcularPrecio: (producto, cantidad) => {
+      return producto.precio_unitario * cantidad;
+    },
+  },
+  {
+    nombre: 'Producto de Descuento Especial',
+    identificador: 'SP',
+    calcularPrecio: (producto, cantidad) => {
+      const descuento = Math.floor(cantidad / 3) * 0.2;
+      const total = producto.precio_unitario * cantidad;
+      return total - total * (descuento > 0.5 ? 0.5 : descuento);
+    },
+  },
+];
+
+export const aplicarReglaPrecio = (
+  producto: Producto,
+  cantidad: number,
+): RespuestaRegla => {
+  const regla = reglasDisponibles.find(
+    (r) => r.identificador === producto.sku.substring(0, 2),
+  );
+  if (!regla) {
+    throw new Error('No hay regla que aplique para este producto');
+  }
+  return {
+    regla,
+    subtotal: regla.calcularPrecio(producto, cantidad),
+  };
+};
--- api/src/core/util/types.ts
+++ api/src/core/util/types.ts
@@ -29,3 +29,14 @@ export type Carrito = {
   items: ItemCarrito[];
   usuario: Usuario;
 };
+
+export type ReglaPrecio = {
+  identificador: string;
+  nombre: string;
+  calcularPrecio: (producto: Producto, cantidad: number) => number;
+};
+
+export type RespuestaRegla = {
+  regla: ReglaPrecio;
+  subtotal: number;
+};
