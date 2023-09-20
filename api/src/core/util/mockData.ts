7644035625067f38b49e67b641896ab60aa24624
MGBrunal <mauriciogarzonb@gmail.com>
2023-09-19 21:49:32 -0500

    feat(api) updates mockData

--- api/src/core/util/mockData.ts
+++ api/src/core/util/mockData.ts
@@ -3,84 +3,55 @@ import { CreateUsuarioDto } from 'src/modules/usuarios/dto';
 
 export const mockedProducts: CreateProductoDto[] = [
   {
-    sku: 'EA123',
-    nombre: 'Smartphone Galaxy S22',
+    sku: 'WE001',
+    nombre: 'Arroz Blanco',
     descripcion:
-      'El último smartphone de Samsung con pantalla AMOLED de alta resolución y cámara de 108 MP.',
-    unidades_disponibles: 50,
-    precio_unitario: 999.99,
-  },
-  {
-    sku: 'WE456',
-    nombre: 'Laptop UltraBook Pro X',
-    descripcion:
-      'Una laptop ultradelgada y potente con procesador Intel Core i7 y pantalla 4K.',
-    unidades_disponibles: 25,
-    precio_unitario: 1499.99,
-  },
-  {
-    sku: 'SP789',
-    nombre: 'Tablet Nexus 10',
-    descripcion:
-      'Tablet Android con pantalla de 10 pulgadas y alto rendimiento para entretenimiento y productividad.',
-    unidades_disponibles: 30,
-    precio_unitario: 349.99,
-  },
-  {
-    sku: 'EA101',
-    nombre: 'Smart TV 4K OLED 55 pulgadas',
-    descripcion:
-      'Televisor OLED con resolución 4K y tecnología de sonido envolvente para una experiencia de visualización inmersiva.',
-    unidades_disponibles: 15,
-    precio_unitario: 1299.99,
-  },
-  {
-    sku: 'WE202',
-    nombre: 'Auriculares Inalámbricos AirPods Pro',
-    descripcion:
-      'Auriculares inalámbricos con cancelación de ruido y calidad de sonido excepcional.',
-    unidades_disponibles: 40,
-    precio_unitario: 199.99,
-  },
-  {
-    sku: 'SP303',
-    nombre: 'Cámara Mirrorless Sony Alpha A7 III',
-    descripcion:
-      'Cámara sin espejo con sensor full-frame y capacidad de grabación de video 4K.',
-    unidades_disponibles: 10,
-    precio_unitario: 1999.99,
-  },
-  {
-    sku: 'EA404',
-    nombre: 'Impresora Multifunción HP OfficeJet Pro',
-    descripcion:
-      'Impresora multifunción con impresión a color de alta calidad y conectividad inalámbrica.',
+      'Bolsa de 1KG Arroz blanco de grano largo ideal para acompañar tus comidas.',
     unidades_disponibles: 20,
-    precio_unitario: 249.99,
+    precio_unitario: 4000,
   },
   {
-    sku: 'WE505',
-    nombre: 'Consola de Videojuegos Xbox Series X',
+    sku: 'WE002',
+    nombre: 'Frijoles Negros',
     descripcion:
-      'La última consola de Microsoft con gráficos de alta definición y capacidad de juego en 4K.',
-    unidades_disponibles: 12,
-    precio_unitario: 499.99,
+      'Bolsa de 1KG Frijoles negros de alta calidad, una excelente fuente de proteína vegetal.',
+    unidades_disponibles: 20,
+    precio_unitario: 2000,
   },
   {
-    sku: 'SP606',
-    nombre: 'Router Wi-Fi Mesh Linksys Velop',
-    descripcion:
-      'Sistema de red Wi-Fi Mesh para una cobertura y velocidad excepcionales en toda la casa.',
-    unidades_disponibles: 18,
-    precio_unitario: 299.99,
+    sku: 'EA003',
+    nombre: 'Aceite de Cocina',
+    descripcion: 'Aceite de cocina vegetal, perfecto para cocinar y freír.',
+    unidades_disponibles: 6,
+    precio_unitario: 12000,
   },
   {
-    sku: 'EA707',
-    nombre: 'Smartwatch Apple Watch Series 7',
-    descripcion:
-      'Reloj inteligente con seguimiento de salud avanzado y pantalla siempre encendida.',
-    unidades_disponibles: 35,
-    precio_unitario: 349.99,
+    sku: 'EA004',
+    nombre: 'Huevos',
+    descripcion: 'Docena de huevos frescos de granja.',
+    unidades_disponibles: 100,
+    precio_unitario: 10000,
+  },
+  {
+    sku: 'SP005',
+    nombre: 'Leche Entera',
+    descripcion: 'Leche entera fresca en envase de 1 litro.',
+    unidades_disponibles: 10,
+    precio_unitario: 4500,
+  },
+  {
+    sku: 'SP009',
+    nombre: 'Papel Higiénico',
+    descripcion: 'Rollo de papel higiénico suave y resistente.',
+    unidades_disponibles: 24,
+    precio_unitario: 2000,
+  },
+  {
+    sku: 'SP010',
+    nombre: 'Jabón de Lavandería',
+    descripcion: 'Jabón en barra para lavar la ropa eficazmente.',
+    unidades_disponibles: 10,
+    precio_unitario: 3500,
   },
 ];
 
